747eee62c7f5bcca57a80be7c3f8c90efca1694f
Andrey Kartashov <akartashov@gilt.com>
2014-11-19 18:44:41 -0500

    Adds TopN utility.

---
+++ src/main/scala/com/gilt/gfc/collection/TopN.scala
@@ -0,0 +1,109 @@
+package com.gilt.gfc.collection
+
+import java.lang.System.arraycopy
+import java.util.{Arrays => JArrays}
+
+import scala.collection.mutable.ArraySeq
+
+/**
+ * Utility to select top N items from a collection.
+ * Given a collection of M items, the aim is to select top N (as defined by some explicit or implicit Ordering).
+ * Basically same as collection.sorted(ordering).take(N).
+ * The point of this class is to avoid sorting entire collection of M items while producing same results.
+ * Implementation assumes that N is relatively small.
+ * {{{
+ * scala> import com.gilt.gfc.collection.TopN
+ * scala> import scala.math.Ordering.Int
+ * scala> val elements = scala.util.Random.shuffle((1 to 10))
+ * scala> val topN = TopN[Int](5, elements)
+ * topN: Seq[Int] = ArraySeq(1, 2, 3, 4, 5)
+ * }}}
+ */
+object TopN {
+  /** Gets the top N items from a given collection. */
+  def apply[T](n: Int,
+               items: TraversableOnce[T])
+              (implicit ord: Ordering[T]): Seq[T] = {
+    require(items != null, "items must not be null")
+    val topN = TopN(n)(ord)
+    topN.addAll(items)
+    topN.toSeq
+  }
+
+  /** Creates TopN object so that items can be added one at a time. */
+  def apply[T](n: Int)
+              (implicit ord: Ordering[T]): TopN[T] = {
+    require(n > 0, "n must be > 0")
+    require(ord != null, "ord must not be null")
+    new TopN(n, new ArraySeq[T](n), ord)
+  }
+
+}
+
+//
+// Not to be constructed directly.
+//
+// The ugly implementation part, roughly
+//   -- sets up an array of N items
+//   -- simply appends to an array while total < N
+//   -- array is sorted when total == N
+//   -- new items are compared against smallest
+//      -- nothing to do if less than smallest
+//      -- binary search, insert, shift the rest if new item is greater than smallest
+//
+//  Implementation assumes that N is small and arraycopy on array of N is very fast.
+//  It prefers element collections where results are closer to the beginning.
+//
+final class TopN[T] private(n: Int,
+                            topItems: ArraySeq[T], // max .. min
+                            ord: Ordering[T]) {
+  private val arrayComparator = ord.asInstanceOf[Ordering[Any]]
+  private val endIdx = n-1
+  private var appendIdx = 0
+
+  /** Adds a single element. */
+  final def add[TT <: T](x: TT) {
+    if (appendIdx > endIdx) {
+      if (arrayComparator.compare(x.asInstanceOf[AnyRef], topItems.array(endIdx)) < 0) { insert(x) }
+    } else {
+      topItems.array(appendIdx) = x.asInstanceOf[AnyRef]
+      if (appendIdx == endIdx) {
+        JArrays.sort(topItems.array, arrayComparator)
+      }
+      appendIdx += 1
+    }
+  }
+
+  /** Adds all elements of a given collection. */
+  final def addAll[TT <: T](xs: TraversableOnce[TT]) {
+    xs.foreach(this.add(_))
+  }
+
+  /** Generates the resulting top N elements. */
+  final def toSeq(): Seq[T] = {
+    if (appendIdx < n) {
+      val items = topItems.slice(0, appendIdx)
+      JArrays.sort(items.array, arrayComparator)
+      items
+    } else {
+      topItems
+    }
+  }
+
+  final private[this] def insert(x: T) {
+    val insertIdx = JArrays.binarySearch(topItems.array, x, arrayComparator)
+
+    val insPoint = if (insertIdx >= 0) {
+      insertIdx
+    } else {
+      -(insertIdx+1)
+    }
+
+    if (insPoint < endIdx) {
+      arraycopy(topItems.array, insPoint, topItems.array, insPoint+1, (endIdx - insPoint))
+    }
+    if (insPoint < n) {
+      topItems.array(insPoint) = x.asInstanceOf[AnyRef]
+    }
+  }
+}
---
+++ src/test/scala/com/gilt/gfc/collection/TopNTest.scala
@@ -0,0 +1,75 @@
+package com.gilt.gfc.collection
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.prop.Checkers
+import org.scalatest.{Matchers, FunSuite}
+
+import scala.math.Ordering
+
+class TopNTest extends FunSuite with Matchers with Checkers {
+
+  import scala.math.Ordering.{ String => StringOrdering }
+
+  val TestItems = scala.util.Random.shuffle(('a' to 'z').map(_.toString)).toSeq
+
+  test("FewerThanN") {
+    val input = TestItems.take(3)
+    val top5 = TopN(5, input)
+    top5 should equal(input.sorted(StringOrdering))
+  }
+
+  test("FewerThanNReverseOrder") {
+    val input = TestItems.take(3)
+    val top5 = TopN(5, input)(StringOrdering.reverse)
+    top5 should equal(input.sorted(StringOrdering.reverse))
+  }
+
+  test("ExactlyN") {
+    val input = TestItems.take(5)
+    val top5 = TopN(5, input)
+    top5 should equal(input.sorted(StringOrdering))
+  }
+
+  test("ExactlyNReverseOrder") {
+    val input = TestItems.take(5)
+    val top5 = TopN(5, input)(StringOrdering.reverse)
+    top5 should equal(input.sorted(StringOrdering.reverse))
+  }
+
+  test("TopN") {
+    val top5 = TopN(5, TestItems)
+    top5 should equal(TestItems.sorted(StringOrdering).take(5))
+  }
+
+  test("TopNReverseOrder") {
+    val top5 = TopN(5, TestItems)(StringOrdering.reverse)
+    top5 should equal(TestItems.sorted(StringOrdering.reverse).take(5))
+  }
+
+  test("RandomizedInputArrayInt") {
+    check { (input: Array[Int], i: Int) =>
+      val n: Int = math.abs(i % 1000) + 1
+      val topN = TopN(n, input)
+      val expected = input.toSeq.sorted.take(n)
+      topN == expected
+    }
+  }
+
+  test("RandomizedInputArrayLongReverse") {
+    check { (input: Array[Long], i: Int) =>
+      val n: Int = math.abs(i % 1000) + 1
+      val topN = TopN(n, input){Ordering.Long.reverse}
+      val expected = input.toSeq.sorted(Ordering.Long.reverse).take(n)
+      topN == expected
+    }
+  }
+
+  test("RandomizedInputListString") {
+    check { (input: List[String], i: Int) =>
+      val n: Int = math.abs(i % 1000) + 1
+      val topN = TopN(n, input)
+      val expected = input.toSeq.sorted.take(n)
+      topN == expected
+    }
+  }
+}
